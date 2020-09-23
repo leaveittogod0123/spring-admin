3b736f90caf4a9a135915615ba67ccf70c9f0347
yosephNoh <yoseph.noh@solarconnect.kr>
2020-09-24 01:41:10 +0900

    상품 등록 API

--- src/main/java/com/noyo0123/backoffice/controller/api/ItemApiController.java
+++ src/main/java/com/noyo0123/backoffice/controller/api/ItemApiController.java
@@ -4,17 +4,24 @@ import com.noyo0123.backoffice.controller.ifs.CrudInterface;
 import com.noyo0123.backoffice.model.network.Header;
 import com.noyo0123.backoffice.model.network.request.ItemApiRequest;
 import com.noyo0123.backoffice.model.network.response.ItemApiResponse;
+import com.noyo0123.backoffice.service.ItemApiService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/item")
 public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+
+    @Autowired
+    private ItemApiService itemApiService;
+
     @Override
     @PostMapping("") // /api/item
     public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> req) {
-        return null;
+        log.info("item create {}", req);
+        return itemApiService.create(req);
     }
 
     @Override
---
+++ src/main/java/com/noyo0123/backoffice/service/ItemApiService.java
@@ -0,0 +1,75 @@
+package com.noyo0123.backoffice.service;
+
+import com.noyo0123.backoffice.controller.ifs.CrudInterface;
+import com.noyo0123.backoffice.model.entity.Item;
+import com.noyo0123.backoffice.model.network.Header;
+import com.noyo0123.backoffice.model.network.request.ItemApiRequest;
+import com.noyo0123.backoffice.model.network.response.ItemApiResponse;
+import com.noyo0123.backoffice.repository.ItemRepository;
+import com.noyo0123.backoffice.repository.PartnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ItemApiService implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+
+    @Override
+    public Header<ItemApiResponse> create(Header<ItemApiRequest> req) {
+
+        ItemApiRequest itemApiRequest = req.getData();
+
+        Item item = Item.builder()
+                .status(itemApiRequest.getStatus())
+                .name(itemApiRequest.getName())
+                .title(itemApiRequest.getTitle())
+                .content(itemApiRequest.getContent())
+                .price(itemApiRequest.getPrice())
+                .brandName(itemApiRequest.getBrandName())
+                .registeredAt(LocalDateTime.now())
+                .partner(partnerRepository.getOne(itemApiRequest.getPartnerId())).build();
+        Item newItem = itemRepository.save(item);
+        return response(newItem);
+    }
+
+    @Override
+    public Header<ItemApiResponse> read(Long id) {
+        return null;
+    }
+
+    @Override
+    public Header<ItemApiResponse> update(Header<ItemApiRequest> req) {
+        return null;
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return null;
+    }
+
+    //response로 item 객체를 리턴하기 때문에 공통 메서드로 생성
+    private Header<ItemApiResponse> response(Item item) {
+        ItemApiResponse itemApiResponse = ItemApiResponse.builder()
+                .id(item.getId())
+                .status(item.getStatus())
+                .name(item.getName())
+                .content(item.getContent())
+                .price(item.getPrice())
+                .brandName(item.getBrandName())
+                .registeredAt(item.getRegisteredAt())
+                .unregisteredAt(item.getUnregisteredAt())
+                .partnerId(item.getPartner().getId())
+                .build();
+
+        return Header.OK(itemApiResponse);
+    }
+}
